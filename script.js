4783f5e9f370f3461ae72bddfb713ec262d84941
Parameshwar Hazra <parameshwarhazra2001@gmail.com>
2024-11-15 22:03:57 +0530

    Initial

---
+++ script.js
@@ -0,0 +1,20 @@
+function Fetch() {
+    fetch(
+        "https://www.timeapi.io/api/time/current/zone?timeZone=Europe%2FAmsterdam"
+    )
+        .then((response) => {
+        return response.json();
+    })
+    .then(response => {
+        response.json().then((data)=> {
+
+            document.getElementById("time").innerText = data.hour + ":" + data.minute + ":" + data.seconds
+        });
+        
+    }). catch(error => {
+        console.log(error);
+        clearInterval(coroutine);
+    });
+
+}
+const coroutine = setInterval(Fetch, 1000);
